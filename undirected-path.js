e7230d00173d107d2215824b31eb9b856ce69edd
Eric Rennie <erennie97@gmail.com>
2023-10-17 10:35:46 -0400

    undirected-path

---
+++ undirected-path.js
@@ -0,0 +1,32 @@
+// Write a function, undirectedPath, that takes in an array of edges for an undirected graph and two nodes (nodeA, nodeB). 
+// The function should return a boolean indicating whether or not there exists a path between nodeA and nodeB.
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+    // create the adjacent list
+    const graph = buildGraph(edges);
+
+};
+
+const buildGraph = (edges) => {
+    //empty graph object
+    const graph = {};
+    for (let edge of edges) {
+        const [ a, b ] = edge;
+        if (!(a in graph)) graph[a] = [];
+        if (!(b in graph)) graph[b] = []; 
+        graph[a].push(b);
+        graph[b].push(a);  
+    }
+
+return graph;
+}
+
+const edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+];
+
+console.log(undirectedPath(edges, 'j', 'm')); // -> true
