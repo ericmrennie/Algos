6bbe1dee848e630741c5e2e95720e555d4dd7da0
Eric Rennie <erennie97@gmail.com>
2024-01-23 17:56:00 -0500

    diagonal difference complete

--- Questions/diagonal-difference.js
+++ Questions/diagonal-difference.js
@@ -7,15 +7,20 @@ const diagonalDifference = (arr) => {
     let diagonalOne = 0;
     // variable to store the sum of diagonal 2
     let diagonalTwo = 0;
-    // iterate through the matrix to find the sum of diagonal 1
+    // iterate through the matrix to find the sum of diagonal 1 & 2
+    let storedVal = arr.length - 1;
+    console.log(storedVal)
     for (let i = 0; i < arr.length; i++) {
         diagonalOne += arr[i][i];
+        diagonalTwo += arr[storedVal][i];
+        storedVal--;
     }
-    // iterate through the matrix to find the sum of diagonal 2
     // return the absolute difference of diagonal 1 and diagonal 2
+    return Math.abs(diagonalOne - diagonalTwo);
 }
 
-console.log(diagonalDifference(3
-    11 2 4
-    4 5 6
-    10 8 -12))
+console.log(diagonalDifference([
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+]));
