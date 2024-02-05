01c4c899395ccd049fa1057c003a3a79d17e39a2
Eric Rennie <erennie97@gmail.com>
2024-02-05 13:22:22 -0500

    xor-strings-2

---
+++ Questions/xor-strings-2.js
@@ -0,0 +1,5 @@
+/*
+Given two strings consisting of digits 0 and 1 only, find the XOR of the two strings.
+
+Debug the given function strings_xor to find the XOR of the two given strings appropriately.
+*/
